d2b290c2100209d4bb9a1548de4080f41bf25f1d
achmafooma <achmafooma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-03 03:17:56 +0000

    first development checkin for v2.0
    git-svn-id: https://plugins.svn.wordpress.org/gallery-shortcode-style-to-head/trunk@236802 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gallery-shortcode-style-to-head.php
+++ gallery-shortcode-style-to-head.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Gallery Shortcode Style to Head
 Plugin URI: http://www.scottbradford.us/software/gallery-shortcode-style-to-head/
-Description: Moves the gallery shortcode styles to the head so it doesn't break XHTML validation
+Description: Moves the gallery shortcode styles to the head so it doesn't break XHTML validation; allows disabling or modifying the default gallery styles. 
 Author: Scott Bradford
 Author URI: http://www.scottbradford.us/
-Version: 1.3
+Version: 2.0
 
         Copyright (c) 2008 Matt Martz (http://sivel.net) (original author)
         Copyright (c) 2009-2010 Scott Bradford (http://www.scottbradford.us) (current maintainer)
@@ -14,8 +14,50 @@ Version: 1.3
 	http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
+// define the default styles
+$float = $wp_locale->text_direction == 'rtl' ? 'right' : 'left';
+$defStyle = ".gallery { margin: auto; }
+.gallery .gallery-item { float: {$float}; margin-top: 10px; text-align: center; }
+.gallery img { border: 2px solid #cfcfcf; }
+.gallery .gallery-caption { margin-left: 0; }";
+
+// These functions add a check box to the media settings page in wp-admin so you can disable the default styles entirely
+// That way, you can put the styles in your template stylesheet(s) [where they belong] if you want.
+
+// initialize the settings variables
+add_action('admin_init', 'add_gssth_setting' );
+function add_gssth_setting() {
+	add_settings_section('gssth','Gallery CSS Styles','','media');
+	add_settings_field('gssth_disable_gallery_style','Disable gallery CSS in \'head\'','build_disable_gallery_styles','media','gssth');
+	add_settings_field('gssth_override_gallery_style','Modify gallery CSS style','build_override_gallery_styles','media','gssth');
+	register_setting( 'media', 'gssth_disable_gallery_style' );
+	register_setting( 'media', 'gssth_override_gallery_style' );
+}
+
+// handle the disable/enable check box
+function build_disable_gallery_styles() {
+	$checked = "";
+	if (get_option('gssth_disable_gallery_style'))
+		$checked=" checked='checked'";
+	echo "<fieldset><legend class=\"screen-reader-text\"><span>Disable the default gallery CSS styles</span></legend>
+<label for=\"disable_gallery_styles\"><input name=\"gssth_disable_gallery_style\" type=\"checkbox\" id=\"gssth_disable_gallery_style\" value=\"1\"" . $checked . " /> Disable the default gallery CSS styles (so you can handle it in your template stylesheets)</label>
+</fieldset>";
+}
+
+// handle the style override field
+function build_override_gallery_styles() {
+	global $defStyle;
+	if (get_option('gssth_override_gallery_style'))
+		$content = get_option('gssth_override_gallery_style');
+	else
+		$content = $defStyle;
+	echo "<textarea style='font-size: 90%; width:95%;' name='gssth_override_gallery_style' id='gssth_override_gallery_style' rows='15' >" . $content . "</textarea>";
+}
+
+
 // This function is largely taken from media.php with manual patches based off of 
 // http://trac.wordpress.org/attachment/ticket/6380/6380-style.diff
+
 function gallery_shortcode_style_out ( $attr ) {
 	global $post, $wp_locale;
 
@@ -80,11 +122,10 @@ function gallery_shortcode_style_out ( $attr ) {
 	$captiontag = tag_escape($captiontag);
 	$columns = intval($columns);
 	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
-	
+
 	$selector = "gallery-{$instance}";
 
-	$output = "<!-- see gallery_shortcode() in wp-includes/media.php -->
-		<div id='$selector' class='gallery galleryid-{$id}'>";
+	$output = apply_filters('gallery_style', "<div id='$selector' class='gallery galleryid-{$id}'>");
 
 	$i = 0;
 	foreach ( $attachments as $id => $attachment ) {
@@ -116,47 +157,41 @@ function gallery_shortcode_style_out ( $attr ) {
 // Default gallery style take from media.php with .gallery-item width removed
 // .gallery-item width applied inline in gallery_shortcode_style_out().
 function gallery_style () { 
-
-$float = $wp_locale->text_direction == 'rtl' ? 'right' : 'left'; 
-$output = apply_filters('gallery_style', "<!-- [gallery] shortcode style -->
+	global $defStyle;
+	$output = "
+<!-- Gallery Shortcode Style to Head 2.0 -->
 <style type='text/css'>
-	.gallery {
-		margin: auto;
+";
+	if (get_option('gssth_override_gallery_style')) { // if the style is saved, export the saved style
+		$output .= get_option('gssth_override_gallery_style');		
+	} else { // if we don't have any styles set right now
+		$output .= $defStyle;
 	}
-	.gallery .gallery-item {
-		float: {$float};
-		margin-top: 10px;
-		text-align: center;
-	}
-	.gallery img {
-		border: 2px solid #cfcfcf;
-	}
-	.gallery-caption {
-		margin-left: 0;
-	}
-</style>");
- 
-echo $output;
+
+	$output .= "
+</style>";
+	echo $output;
 } 
 
 // Look ahead to check if any posts contain the [gallery] shortcode
 // if true then add default gallery style to head
 function gallery_scan () { 
-        global $posts; 
+	global $posts; 
  
-        if ( !is_array ( $posts ) ) 
-                return; 
+    if ( !is_array ( $posts ) ) 
+    	return; 
  
-        foreach ( $posts as $post ) { 
-                if ( false !== strpos ( $post->post_content, '[gallery' ) ) { 
-                        add_action ( 'wp_head', 'gallery_style' ); 
-                        break; 
-                } 
-        } 
+    foreach ( $posts as $post ) { 
+           if ( false !== strpos ( $post->post_content, '[gallery' ) ) { 
+                   add_action ( 'wp_head', 'gallery_style' ); 
+                   break; 
+           } 
+    } 
 } 
 
 // Tell WordPress what to do
 remove_shortcode ( 'gallery_shortcode' );			// Remove included WordPress [gallery] shortcode function
 add_shortcode ( 'gallery' , 'gallery_shortcode_style_out' );	// Add new [gallery] shortcode function
-add_action ( 'template_redirect' , 'gallery_scan' );		// Add look ahead for [gallery] shortcode
+if (!get_option('gssth_disable_gallery_style'))  // don't do the look-ahead if styles are disabled
+	add_action ( 'template_redirect' , 'gallery_scan' );		// Add look ahead for [gallery] shortcode
 ?>
