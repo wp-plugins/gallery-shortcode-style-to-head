0e284f96f4b2c2dab1fc54fd11547ea535b9b680
achmafooma <achmafooma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-15 21:22:53 +0000

    Version 2.1 Update (including WP 3.1 support)
    git-svn-id: https://plugins.svn.wordpress.org/gallery-shortcode-style-to-head/tags/2.1@333009 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gallery-shortcode-style-to-head.php
@@ -0,0 +1,203 @@
+<?php
+/*
+Plugin Name: Gallery Shortcode Style to Head
+Plugin URI: http://www.scottbradford.us/software/gallery-shortcode-style-to-head/
+Description: Moves the gallery shortcode styles to the head so it doesn't break XHTML validation; allows disabling or modifying the default gallery styles. 
+Author: Scott Bradford
+Author URI: http://www.scottbradford.us/
+Version: 2.1
+
+    Copyright (c) 2008 Matt Martz (http://sivel.net) (original author)
+    Copyright (c) 2009-2011 Scott Bradford (http://www.scottbradford.us) (current maintainer)
+        
+    Gallery Shortcode Style to Head is released under the GNU General Public License (GPL)
+	http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+// define the default styles
+$float = $wp_locale->text_direction == 'rtl' ? 'right' : 'left';
+$defStyle = ".gallery { margin: auto; }
+.gallery .gallery-item { float: {$float}; margin-top: 10px; text-align: center; }
+.gallery img { border: 2px solid #cfcfcf; }
+.gallery .gallery-caption { margin-left: 0; }";
+
+// These functions add a check box to the media settings page in wp-admin so you can disable the default styles entirely
+// That way, you can put the styles in your template stylesheet(s) [where they belong] if you want.
+
+// initialize the settings variables
+add_action('admin_init', 'add_gssth_setting' );
+function add_gssth_setting() {
+	register_setting( 'media', 'gssth_disable_gallery_style' );
+	register_setting( 'media', 'gssth_override_gallery_style' );
+	add_settings_section('gssth','Gallery CSS Styles','display_gssth_description','media');
+	add_settings_field('gssth_disable_gallery_style','Disable gallery CSS in \'head\'','build_disable_gallery_styles','media','gssth');
+	add_settings_field('gssth_override_gallery_style','Modify gallery CSS style','build_override_gallery_styles','media','gssth');
+}
+
+// display the GSSTH description
+function display_gssth_description() {
+	echo "<p>Override or disable the default WordPress gallery styles. To reset to default styles, un-check the disable option, clear out the style code (so it is completely empty), and save the changes.</p>";
+}
+
+// handle the disable/enable check box
+function build_disable_gallery_styles() {
+	$checked = "";
+	if (get_option('gssth_disable_gallery_style'))
+		$checked=" checked='checked'";
+	echo "<fieldset><legend class=\"screen-reader-text\"><span>Disable the default gallery CSS styles</span></legend>
+<label for=\"disable_gallery_styles\"><input name=\"gssth_disable_gallery_style\" type=\"checkbox\" id=\"gssth_disable_gallery_style\" value=\"1\"" . $checked . " /> Disable the default gallery CSS styles (so you can handle it in your template stylesheets)</label>
+</fieldset>";
+}
+
+// handle the style override field
+function build_override_gallery_styles() {
+	global $defStyle;
+	if (get_option('gssth_override_gallery_style'))
+		$content = get_option('gssth_override_gallery_style');
+	else
+		$content = $defStyle;
+	echo "<textarea style='font-size: 90%; width:95%;' name='gssth_override_gallery_style' id='gssth_override_gallery_style' rows='15' >" . $content . "</textarea>";
+}
+
+
+// This function is largely taken from media.php with manual patches based off of 
+// http://trac.wordpress.org/attachment/ticket/6380/6380-style.diff
+
+function gallery_shortcode_style_out ( $attr ) {
+	global $post, $wp_locale;
+
+	static $instance = 0;
+	$instance++;
+
+	// Allow plugins/themes to override the default gallery template.
+	$output = apply_filters('post_gallery', '', $attr);
+	if ( $output != '' )
+		return $output;
+
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => 'dl',
+		'icontag'    => 'dt',
+		'captiontag' => 'dd',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'include'    => '',
+		'exclude'    => ''
+	), $attr));
+
+	$id = intval($id);
+	if ( 'RAND' == $order )
+		$orderby = 'none';
+
+	if ( !empty($include) ) {
+		$include = preg_replace( '/[^0-9,]+/', '', $include );
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[$val->ID] = $_attachments[$key];
+		}
+	} elseif ( !empty($exclude) ) {
+		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	} else {
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	}
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+
+	$selector = "gallery-{$instance}";
+	$size_class = sanitize_html_class( $size );
+
+	$output = apply_filters('gallery_style', "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>");
+
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+		$output .= "<{$itemtag} class='gallery-item' style='width:{$itemwidth}%'>";
+		$output .= "
+			<{$icontag} class='gallery-icon'>
+				$link
+			</{$icontag}>";
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
+			$output .= "
+				<{$captiontag} class='wp-caption-text gallery-caption'>
+				" . wptexturize($attachment->post_excerpt) . "
+				</{$captiontag}>";
+		}
+		$output .= "</{$itemtag}>";
+		if ( $columns > 0 && ++$i % $columns == 0 )
+			$output .= '<div style="clear: both"></div>';
+	}
+
+	$output .= "
+			<div style='clear: both;'></div>
+		</div>\n";
+
+	return $output;
+}
+
+// Default gallery style taken from media.php with .gallery-item width removed
+// .gallery-item width applied inline in gallery_shortcode_style_out().
+function gallery_style () { 
+	global $defStyle;
+	$output = "
+<!-- Gallery Shortcode Style to Head 2.1 -->
+<style type='text/css'>
+";
+	if (get_option('gssth_override_gallery_style')) { // if the style is saved, export the saved style
+		$output .= get_option('gssth_override_gallery_style');		
+	} else { // if we don't have any styles set right now
+		$output .= $defStyle;
+	}
+
+	$output .= "
+</style>";
+	echo $output;
+} 
+
+// Look ahead to check if any posts contain the [gallery] shortcode
+// if true then add default gallery style to head
+function gallery_scan () { 
+	global $posts; 
+ 
+    if ( !is_array ( $posts ) ) 
+    	return; 
+ 
+    foreach ( $posts as $post ) { 
+           if ( false !== strpos ( $post->post_content, '[gallery' ) ) { 
+                   add_action ( 'wp_head', 'gallery_style' ); 
+                   break; 
+           } 
+    } 
+} 
+
+// Tell WordPress what to do
+remove_shortcode ( 'gallery_shortcode' );			// Remove included WordPress [gallery] shortcode function
+add_shortcode ( 'gallery' , 'gallery_shortcode_style_out' );	// Add new [gallery] shortcode function
+if (!get_option('gssth_disable_gallery_style'))  // don't do the look-ahead if styles are disabled
+	add_action ( 'template_redirect' , 'gallery_scan' );		// Add look ahead for [gallery] shortcode
+?>
---
+++ readme.txt
@@ -0,0 +1,102 @@
+=== Gallery Shortcode Style to Head ===
+Contributors: achmafooma, sivel
+Tags: gallery, shortcode, style, css, xhtml, head, validation
+Requires at least: 2.9
+Tested up to: 3.1
+Stable tag: 2.1
+
+Moves the gallery shortcode styles to the head so it doesn't break XHTML
+validation; allows disabling or modifying the default gallery styles. 
+
+== Description ==
+
+Moves the gallery shortcode styles to the head so it doesn't break XHTML
+validation; allows disabling or modifying the default gallery styles.
+
+By default when using the WordPress gallery, the styles are placed into the
+post content which breaks XHTML validation. This plugin moves the style into
+the head of the page using a look-ahead to determine if the [gallery]
+shortcode is used in any posts.
+
+This plugin also gives you the option to modify the default gallery style
+CSS or disable the gallery styles entirely (so you can control it from your
+template CSS files).
+
+This plugin uses ideas recommended in a patch located at
+http://trac.wordpress.org/attachment/ticket/6380/6380-style.diff
+
+Special thanks to the original author of this plugin, Matt Martz, http://sivel.net.
+
+== Installation ==
+
+Installation is just like any other WordPress plugin:
+
+1. Upload the 'gallery-shortcode-style-to-head' folder to the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+Immediately after installation, your galleries should pass W3C XHTML validation.
+You can make further adjustments under Admin > Settings > Media.
+
+== Frequently Asked Questions ==
+
+= How do I make the WP galleries XHTML compliant? =
+
+1. Just activate the plugin. Nothing else is required.
+
+= How do I adjust the default gallery CSS styles? =
+
+1. In WP admin, go to Settings > Media.
+2. Scroll down to "Modify gallery CSS style.'"
+3. Adjust to your liking!
+4. Click "Save Changes."
+
+= How do I disable the CSS styles (so I can control the CSS in my template)? =
+
+1. In WP admin, go to Settings > Media.
+2. Scroll down to "Disable gallery CSS in 'head.'"
+3. Check the box.
+4. Click "Save Changes."
+
+= How do I reset the gallery CSS styles to default? =
+
+1. In WP admin, go to Settings > Media.
+2. Un-check the setting for "Disable gallery CSS in 'head'."
+3. Clear everything in the "Modify gallery CSS style" text field (so it's totally empty).
+4. Click "Save Changes." The styles will be reset to default.
+
+== Screenshots ==
+
+1. Plugin settings (under Admin > Settings > Media).
+
+== Changelog ==
+
+= 2.1 (2011-01-15): =
+* Removed clearing <br> tags in galleries (replaced with clearing <div> tags).
+* Corrected bug that threw a cryptic error on the media page for some users.
+* Support for WordPress 3.1; now requires WordPress 2.9 or higher.
+
+= 2.0 (2010-05-02): =
+* New gallery style settings on the Admin > Settings > Media page.
+* Ability to modify the default gallery styles.
+* Ability to disable the default styles entirely (so you can style the gallery in your template CSS).
+* Support for WordPress 3.0; now requires WordPress 2.7 or higher.
+
+= 1.3 (2010-02-09): =
+* Re-Sync with WordPress 2.9 gallery code for support of the new 'include' and 'exclude' options.
+
+= 1.2 (2009-09-20): =
+* Corrected function of 'Link thumbnails to...' setting on galleries when this plugin is enabled.
+
+= 1.1 (2008-09-22): =
+* Added filter so that the default style can be overridden.
+
+= 1.0 (2008-08-27): =
+* Initial Public Release
+
+== Upgrade Notice ==
+
+= 2.1 =
+Bugfixes, re-sync with WP 3.1 code, and support for WP 3.1.
+
+= 2.0 =
+Adds ability to modify or disable the default styles from WP admin; supports WP 3.0.
